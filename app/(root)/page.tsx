6849479ea322ee0db6105646ae9e190e9024ef61
ogberagabriel234@gmail.com <ogberagabriel234@gmail.com>
2024-05-06 15:18:52 +0100

    Home Page Loading

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+import React from "react";
 
 const Home = () => {
+  const loggedIn = {firstname: "xGuest"}
   return (
-    <div>Home</div>
-  )
-}
+    <section className="home">
+      <div className="home-content">
+        <HeaderBox 
+         type="greeting"
+         title="Welcome"
+         user={loggedIn?.firstname || "xXx.Fresh"}
+         subtext="Access and manage your acccount and transactions efficiently"
+        />
+        <TotalBalanceBox 
+         accounts={[]}
+         totalBanks={1}
+         totalCurrentBalance={1250.35}
+        />
+      </div>
+    </section>
+  );
+};
 
-export default Home
+export default Home;
