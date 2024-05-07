ff380489d70187e9dc68cb85c8bb1f86aa867419
ogberagabriel234@gmail.com <ogberagabriel234@gmail.com>
2024-05-07 16:37:49 +0100

    implemented sidebar for all devices

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,7 @@
-import { Children } from "react";
+import MobileNavbar from "@/components/MobileNavbar";
+import Sidebar from "@/components/Sidebar";
+import Image from "next/image";
+
 
 ;
 
@@ -7,10 +10,24 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = {firstname: "XGuest", lastname: "xXxaiver"}
   return (
-    <main>
-        SIDEBAR
-        {children}
+    <main className="flex h-screen w-full font-inter">
+        <Sidebar user={loggedIn}/>
+        <div className="flex size-full flex-col">
+          <div className="root-layout">
+            <Image 
+             src="/icons/logo.svg"
+             width={30}
+             height={30}
+             alt="menu-logo"
+            />
+            <div>
+              <MobileNavbar user={loggedIn}/>
+            </div>
+          </div>
+          {children}
+        </div>
     </main>
   );
 }
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,24 +1,30 @@
 import HeaderBox from "@/components/HeaderBox";
+import RightSideBar from "@/components/RightSideBar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
 const Home = () => {
-  const loggedIn = {firstname: "xGuest"}
+  const loggedIn = {
+    firstName: "Gaboski",
+    lastName: "Ogbera",
+    email: "xguest@gmail.com",
+  };
   return (
     <section className="home">
       <div className="home-content">
-        <HeaderBox 
-         type="greeting"
-         title="Welcome"
-         user={loggedIn?.firstname || "xXx.Fresh"}
-         subtext="Access and manage your acccount and transactions efficiently"
+        <HeaderBox
+          type="greeting"
+          title="Welcome"
+          user={loggedIn?.firstName || "xXx.Fresh"}
+          subtext="Access and manage your acccount and transactions efficiently"
         />
-        <TotalBalanceBox 
-         accounts={[]}
-         totalBanks={1}
-         totalCurrentBalance={1250.35}
+        <TotalBalanceBox
+          accounts={[]}
+          totalBanks={1}
+          totalCurrentBalance={1250.35}
         />
       </div>
+      <RightSideBar user={loggedIn} transactions={[]} banks={[{ currentBalance: 1250.35 }, {currentBalance: 1250.70}]} />
     </section>
   );
 };
