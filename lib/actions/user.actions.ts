0a1aeb0223ebd019dbb448366c7dcdcc5fc67df7
ogberagabriel234@gmail.com <ogberagabriel234@gmail.com>
2024-05-22 14:49:31 +0100

    complete the other fecthing bank logic

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -240,3 +240,38 @@ export const getBanks = async ({ userId } : getBanksProps) => {
     console.error(error);
   }
 }
+
+
+export const getBank = async ({documentId} : getBankProps) => {
+  try {
+    const { database } = await createAdminClient();
+
+    const bank = await database.listDocuments(
+      DATABASE_ID!,
+      BANK_COLLECTION_ID!,
+      [Query.equal('$id', [documentId])]
+    )
+
+    return parseStringify(bank.documents[0]);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export const getBanksByAccountId = async ({ accountId}: getBankByAccountIdProps) => {
+  try {
+    const { database } = await createAdminClient();
+
+    const bank = await database.listDocuments(
+      DATABASE_ID!,
+      BANK_COLLECTION_ID!,
+      [Query.equal('accountId', [accountId])]
+    )
+
+    if(bank.total !== 1) return null;
+
+    return parseStringify(bank.documents[0]);
+  } catch (error) {
+    console.error(error);
+  }
+}
