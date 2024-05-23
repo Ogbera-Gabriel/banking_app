9387e9df30afc207917b0d6d612939a58198cff0
ogberagabriel234@gmail.com <ogberagabriel234@gmail.com>
2024-05-24 00:55:01 +0100

    implemented rendering transactions

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,6 @@
 import HeaderBox from "@/components/HeaderBox";
+import RecentTransactions from "@/components/RecentTransactions";
+
 import RightSideBar from "@/components/RightSideBar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
 import Loading from "@/components/loading";
@@ -18,11 +20,12 @@ const Home = async ({ searchParams: { id, page } }: SearchParamProps) => {
 
   const account = await getAccount({ appwriteItemId });
 
-  console.log(account);
+  
 
   return (
     <section className="home">
       <div className="home-content">
+        <header className="home-header">
         <HeaderBox
           type="greeting"
           title="Welcome"
@@ -34,7 +37,17 @@ const Home = async ({ searchParams: { id, page } }: SearchParamProps) => {
           totalBanks={accounts?.totalBanks}
           totalCurrentBalance={accounts?.totalCurrentBalance}
         />
+        </header>
+
+        <RecentTransactions 
+          accounts={accountsData}
+          transactions={account?.transactions}
+          appwriteItemId={appwriteItemId}
+          page={currentPage}
+        />
       </div>
+       
+      
       <RightSideBar
         user={loggedIn}
         transactions={accounts?.transactions}
